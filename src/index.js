6dbc02671dbe534fc5ab4d35dd53e5d1fe3c1bc5
Shamp07 <blog.shamp@gmail.com>
2019-11-06 18:10:20 +0900

    feat: 헤더 추가중

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import stores from './stores';
 import history from './history';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render((
   <Provider
