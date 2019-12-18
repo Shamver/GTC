348c69f39f85b41a44466c9c65884dea32301cac
shamp07 <blog.shamp@gmail.com>
2019-12-18 22:46:21 +0900

    feat: freeBoard bug fix

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router';
 import styled from 'styled-components';
 import Main from '../Content/Main';
 import Posting from '../Content/Posting';
+import FreeBoard from '../Content/FreeBoard';
 // import PostList from '../Content/PostList';
 import Alert from '../util/Alert';
 import Sign from '../util/Sign';
@@ -19,9 +20,8 @@ const Content = () => (
     <Switch>
       <Route exact path="/" render={() => <Main />} />
       <Route exact path="/:board/post" render={({ match }) => <Posting match={match} />} />
-      <Route exact path="/free" render={() => <Contents.FreeBoard />} />
+      <Route exact path="/free" render={({ location }) => <Contents.FreeBoard location={location} />} />
       <Route exact path="/settings" render={() => <Contents.Settings />} />
-      <Route exact path="/posts/:postId" render={() => <Contents.FreeBoard />} />
     </Switch>
     <Alert />
     <Sign />
