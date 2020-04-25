8deecba882e0f0383071f583bfdb76ad41fb7800
shamp07 <blog.shamp@gmail.com>
2020-04-25 21:39:51 +0900

    feat: 광고 밸리데이션

--- server/routes/board/BoardRouter.js
+++ server/routes/board/BoardRouter.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const { error, info } = require('../../log-config');
+const { info } = require('../../log-config');
 
 const Database = require('../../Database');
 
 const SELECT_CURRENT_BOARD = `
-  SELECT LOWER(B_ID) AS currentBoard 
-  FROM GTC_BOARD_POST
-  WHERE ID = :ID
+  SELECT LOWER(BOARD_CD) AS currentBoard 
+  FROM GTC_POST
+  WHERE ID = :POST_ID
 `;
 
 router.get('/', (req, res) => {
@@ -19,33 +19,19 @@ router.get('/', (req, res) => {
     (database) => database.query(
       SELECT_CURRENT_BOARD,
       {
-        ID: id,
+        POST_ID: id,
       },
     )
       .then((rows) => {
         res.json({
           SUCCESS: true,
-          CODE: 1,
-          MESSAGE: '현재 게시판 로딩',
+          CODE: 0,
+          MESSAGE: '현재 게시판 조회 성공',
           DATA: rows[0].currentBoard,
         });
       }),
   ).then(() => {
-    // 한 DB 트랜잭션이 끝나고 하고 싶은 짓.
     info('[SELECT, GET /api/board] 현재 게시판 조회');
-  }).catch((err) => {
-    // 트랜잭션 중 에러가 났을때 처리.
-    error(err.message);
-
-    // Database 에서 보여주는 에러 메시지
-    if (err.sqlMessage) {
-      error(err.sqlMessage);
-    }
-
-    // 실행된 sql
-    if (err.sql) {
-      error(err.sql);
-    }
   });
 });
 
