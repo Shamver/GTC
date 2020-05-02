c743f34aef8a62a9e85b5a8360566afe88f83420
shamp07 <blog.shamp@gmail.com>
2020-05-02 23:39:51 +0900

    feat: DropDown onActive 리액트스트랩 구현 애매함

--- src/stores/component/SidebarStore.js
+++ src/stores/component/SidebarStore.js
@@ -9,9 +9,8 @@ class SidebarStore {
     mail: false,
   };
 
+  // 드롭다운을 열때와 드롭다운이 열린뒤에는 어디를 클릭해도 해당 이벤트가 발생한다.
   @action onActive = (event) => {
-    console.log(event.target.classList);
-    console.log(event.target.classList.contains('dropdown-item'));
     if (!event.target.classList.contains('dropdown-item')) {
       event.preventDefault();
     }
@@ -19,13 +18,19 @@ class SidebarStore {
     const currentName = event.target.getAttribute('name');
     const keyList = Object.keys(this.dropdown);
     let key;
+    let result;
 
+    alert(currentName);
     for (let i = 0; i < keyList.length; i += 1) {
       key = keyList[i];
+      result = true;
       if (currentName === key) {
+        if (this.dropdown[currentName]) {
+          result = false;
+        }
         this.dropdown = {
           ...this.dropdown,
-          [key]: true,
+          [key]: result,
         };
       } else {
         this.dropdown = {
