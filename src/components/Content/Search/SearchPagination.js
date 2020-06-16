e2aa389f865b7e48a2c8c49fd0a0085db27f1b96
lisiver <developer.lisiver@gmail.com>
2020-06-16 10:53:40 +0900

    feat: searchPagination 컴포넌트 추가

---
+++ src/components/Content/Search/SearchPagination.js
@@ -0,0 +1,141 @@
+import React, { memo, useEffect } from 'react';
+import { PaginationItem, Pagination } from 'reactstrap';
+import styled from 'styled-components';
+import * as Proptypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import useStores from '../../../stores/useStores';
+
+const PaginationList = ({ currentPage, noPagination }) => {
+  const { BoardSearchStore } = useStores();
+  const { foundMaxPage } = BoardSearchStore;
+
+  const currentPageNum = parseInt(currentPage, 0);
+  const min = (currentPageNum - 3) <= 0 ? 1 : currentPageNum - 3;
+  const max = (currentPageNum + 3) > foundMaxPage ? foundMaxPage : currentPageNum + 3;
+  const array = new Array((max - min) + 1 < 0 ? 0 : (max - min) + 1);
+
+  if (currentPage > 1) {
+    array.push(
+      <PaginationItem key={0}>
+        <CustomLink className="page-link" activeClassName="active" to={`/search/page/${currentPageNum - 1}`}>
+          <SmallSpan>
+            ＜
+          </SmallSpan>
+        </CustomLink>
+      </PaginationItem>,
+    );
+  }
+
+  for (let i = min; i <= max; i += 1) {
+    array.push(
+      <PaginationItem active={i === 1 && noPagination} key={i}>
+        <CustomLink className="page-link" activeClassName="active" to={`/search/page/${i}`}>
+          {i}
+        </CustomLink>
+      </PaginationItem>,
+    );
+  }
+
+  if (currentPageNum !== foundMaxPage) {
+    array.push(
+      <PaginationItem key={-1}>
+        <CustomLink className="page-link" activeClassName="active" to={`/search/page/${currentPageNum + 1}`}>
+          <SmallSpan>
+            ＞
+          </SmallSpan>
+        </CustomLink>
+      </PaginationItem>,
+    );
+  }
+
+  // 추후 max 값 조정후 추가
+  return array;
+};
+
+PaginationList.propTypes = {
+  currentPage: Proptypes.string,
+  noPagination: Proptypes.bool,
+};
+
+PaginationList.defaultProps = {
+  noPagination: false,
+  currentPage: '1',
+};
+
+
+const SearchPagination = ({ noPagination, currentPage }) => {
+  const { BoardSearchStore } = useStores();
+  const { foundMaxPage } = BoardSearchStore;
+
+  useEffect(() => {
+
+  }, [foundMaxPage]);
+
+  return (
+    <PaginationCustom>
+      <PaginationList currentPage={currentPage} noPagination={noPagination} />
+    </PaginationCustom>
+  );
+};
+
+SearchPagination.propTypes = {
+  noPagination: Proptypes.bool,
+  currentPage: Proptypes.string,
+};
+
+SearchPagination.defaultProps = {
+  noPagination: false,
+  currentPage: '1',
+};
+
+const SmallSpan = styled.span`
+  font-size: 0.8rem;
+`;
+
+const CustomLink = styled(NavLink)`
+  &.active {
+    z-index: 1;
+    color: #fff !important;
+    background-color: #DC3545;
+    border-color: #DC3545;
+  }
+  
+  &.active:hover {
+    background-color: #DC3545;
+    border-color: #DC3545;
+  } 
+`;
+
+
+const PaginationCustom = styled(Pagination)`
+  color : #DC3545;
+  width : max-content;
+  margin : 0 auto;
+  margin-top : 20px;
+  
+  & ul {
+    margin : 0px;
+  }
+  
+  & .page-link {
+    color: #DC3545;
+  }
+  
+  & .page-link:hover {
+    color: #DC3545;
+  } 
+  
+  & .page-link:focus {
+    box-shadow: none;
+  } 
+  
+  & .page-item.active .page-link {
+    z-index: 1;
+    color: #fff;
+    background-color: #DC3545;
+    border-color: #DC3545;
+  }
+`;
+
+export default memo(observer(SearchPagination));
