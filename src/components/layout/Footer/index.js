b9e7890428ba5c60e23e93d4f4c7ff02a9c93d84
obeybook <obeybook@gmail.com>
2020-06-29 21:42:33 +0900

    feat: 푸터 영역 관리자 모집, 광고문의 메뉴 삭제

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -15,10 +15,6 @@ const Footer = () => {
       <LinkButton onClick={toggleTos}>이용약관</LinkButton>
       &nbsp; | &nbsp;
       <LinkButton onClick={togglePrivacy}>개인정보보호방침</LinkButton>
-      &nbsp; | &nbsp;
-      <Link to="/team">관리자 모집</Link>
-      &nbsp; | &nbsp;
-      <Link to="/">광고문의</Link>
       <br />
       GTC(Growtopia Community)는 Growtopia에서 운영하는 사이트가 아닙니다. <br />
       문의사항은 shamver0114@gmail.com 로 메일 보내주세요.<br />
