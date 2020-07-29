8ae8d28d4c5302a1f328f4c736bae51a3d32dbfd
obeybook <obeybook@gmail.com>
2020-07-29 10:39:55 +0900

    fix: 머지 작업 중 잘못된 코드 입력 수정

--- src/components/util/Sign.js
+++ src/components/util/Sign.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { observer } from 'mobx-react';
 import {
   Modal, ModalHeader, ModalBody, FormText, Input,
