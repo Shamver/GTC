55d8199f60291083e1e7aa23e1c7ecada629e4f9
YoonEunSik <developer.lisiver@gmail.com>
2020-07-31 21:20:51 +0900

    feat(consult): 1:1 문의 카테고리 추가

--- src/components/layout/Category.js
+++ src/components/layout/Category.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Container, Row } from 'reactstrap';
 import {
   faBars, faFlag, faList, faGlobeAsia, faTshirt, faLock,
-  faComments, faCode,
+  faComments, faCode, faAt,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { observer } from 'mobx-react';
@@ -57,6 +57,11 @@ const Category = () => {
             <FaiPink icon={faComments} className="fa-fw" />&nbsp;&nbsp; 질문&답변
           </MenuDiv>
         </MenuLink>
+        <MenuLink to="/consult" activeClassName="active">
+          <MenuDiv>
+            <FaiPink icon={faAt} className="fa-fw" />&nbsp;&nbsp; 1:1 문의
+          </MenuDiv>
+        </MenuLink>
         {/*
           <MenuLink to="/crime"  activeClassName="active">
             <MenuDiv>
@@ -68,11 +73,6 @@ const Category = () => {
           <FaiPink icon={faQuestion} className="fa-fw" />&nbsp;&nbsp; 자주 묻는 질문
           </MenuDiv>
           </MenuLink>
-        <MenuLink to="/consult" activeClassName="active">
-          <MenuDiv>
-            <FaiPink icon={faAt} className="fa-fw" />&nbsp;&nbsp; 1:1 문의
-          </MenuDiv>
-        </MenuLink>
         */}
         { RouterAuthCheck(3) && (
           <MenuLink to="/code" activeClassName="active">
